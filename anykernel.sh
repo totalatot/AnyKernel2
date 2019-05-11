f92a798d66653990133572a2e8531d661bd5738e
TotalChen <totalatot@gmail.com>
2019-05-11 18:36:53 +0800

    Apply my modification to anykernel for Moto X Pro

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Moto X Pro Kernel by Jingshao@QQ2239298161
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
-device.name3=
-device.name4=
+device.name1=shamu
+device.name2=Shamu
+device.name3=shamu_t
+device.name4=Shamu_t
 device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -40,6 +40,19 @@ dump_boot;
 
 
 # begin ramdisk changes
+
+# init.shamu.rc
+backup_file init.shamu.rc;
+append_file init.shamu.rc "Moto X Pro" init.shamu.add;
+
+# fstab.shamu
+backup_file fstab.shamu;
+replace_file fstab.shamu 750 fstab.shamu.replace;
+
+# ueventd.shamu.rc
+backup_file ueventd.shamu.rc
+append_file ueventd.shamu.rc "Moto Sensors" ueventd.shamu.add;
+
 # end ramdisk changes
 
 write_boot;
---
+++ makekernel.sh
@@ -0,0 +1,3 @@
+zip -r9 Kernel4Pie.zip * -x README.md -x makekernel.sh Kernel4Pie.zip
+mv Kernel4Pie.zip ~/FTP/kernel/Kernel4Pie.zip
+cd ~/FTP/kernel/ && ll
